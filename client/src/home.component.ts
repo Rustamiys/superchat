7e57f04cf71eddb6b030d98f0bf83e399601e453
Kate <112977945+Kate-Borushko@users.noreply.github.com>
2024-12-08 18:58:44 +0300

    home

---
+++ client/src/home.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { AuthenticationService } from '';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+    user$ = this.usersService.currentUserProfile$;
+
+    searchControl = new FormControl('');
+
+    users$ = combineLatest([
+        this.usersService.allUsers$,
+        this.user$,
+        this.searchControl.valueChanges.pipe(startWith(''))
+    ]).pipe(
+        map(([users, user, searchString]) =>  users.filter(u => u.displayName?.toLowerCase().includes(searchString.toLowerCase()) && u.uid !== user?.uid))
+    );
+
+    constructor(private usersService: UsersService, private chatsService: ChatsService) { }
+
+    ngOnInit(): void { }
+
+    createChat(otherUser: ProfileUser) {
+        this.chatsService.createChat(otherUser).subscribe();
+    }
+}
