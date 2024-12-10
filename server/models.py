e0f901e7a7d49f3f8c9b9aad15c2a35628b3c5d2
Rustam <r.shaihutdinov@uptrade.ru>
2024-12-10 19:38:10 +0300

    Добавил модель message

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date
 from database import Base
+from sqlalchemy.orm import Mapped
 
 class User(Base):
     __tablename__ = "users"
@@ -10,3 +11,11 @@ class User(Base):
     birthday = Column(Date)
     username = Column(String, unique=True, index=True)
     password = Column(String)
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id: Mapped[int] = Column(Integer, unique=True, autoincrement=True, primary_key=True)
+    message: Mapped[str] = Column(String)
+    sender_id: Mapped[str] = Column(Integer)
+    recipient_id: Mapped[str] = Column(Integer)
