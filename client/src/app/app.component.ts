b77e63a777cb33f26291e8283bf222e78087c83b
Rustam <r.shaihutdinov@uptrade.ru>
2024-12-11 01:28:29 +0300

    Доработки фронта

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; // Импорт RouterOutlet для маршрутизации
-import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'client'; 
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -3,13 +3,24 @@ import { ReactiveFormsModule, FormControl} from '@angular/forms';
 import { MatFormFieldModule,} from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule],
+  imports: [ReactiveFormsModule, MatDividerModule, MatFormFieldModule, MatListModule,
+    MatInputModule, MatAutocompleteModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  
   searchControl = new FormControl('');
+  messageControl = new FormControl('');
+  chatListControl = new FormControl('');
+  sendMessage(): void {
+    console.log('Message sent!');
+    // Добавьте здесь логику отправки сообщения
+  }
 }
