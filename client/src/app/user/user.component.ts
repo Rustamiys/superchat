efc87a8338ff751236d1027bb39481600b000052
Rustam <r.shaihutdinov@uptrade.ru>
2024-12-12 15:00:57 +0300

    Реистрация

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -5,22 +5,22 @@ import { CommonModule } from '@angular/common';
 import { UserService, User } from '../services/user.service'
 import { HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { HomeComponent } from '../home/home.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  imports: [ReactiveFormsModule, FormsModule, CommonModule, HttpClientModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule, HttpClientModule, HomeComponent],
   providers: [UserService],
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  users: User[] = [];
+export class UserComponent {
   registerForm: FormGroup;
   loginForm: FormGroup;
-
-  message: string = '';
-
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  registerDisplay: boolean = true;
+  loginDisplay: boolean = false; 
+  user_login: string = '';
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {
     this.registerForm = this.fb.group({
       login: ['', [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -30,63 +30,71 @@ export class UserComponent implements OnInit {
     });
 
     this.loginForm = this.fb.group({
-      username: ['', [Validators.required]],
+      login: ['', [Validators.required]],
       password: ['', [Validators.required]]
     });
   }
 
-  ngOnInit(): void {
-    this.getUsers();
-  }
+    // this.getUsers();
 
-  getUsers(): void {
-    this.userService.getAllUsers().subscribe({
-      next: (data) => {
-        this.users = data;
-      },
-      error: (error) => {
-        console.error('Ошибка при загрузке пользователей:', error);
-      },
-    });
-  }
-  
-
-  // // Получение списка пользователей
   // getUsers(): void {
-  //   this.http.get<any[]>('/api/users').subscribe(
-  //     (data) => {
+  //   this.userService.getAllUsers().subscribe({
+  //     next: (data) => {
   //       this.users = data;
   //     },
-  //     (error) => {
-  //       console.error('Ошибка при получении пользователей:', error);
-  //     }
-  //   );
+  //     error: (error) => {
+  //       console.error('Ошибка при загрузке пользователей:', error);
+  //     },
+  //   });
+  //   console.log(this.userService.getAllUsers())
   // }
 
-  // Регистрация нового пользователя
   registerUser(): void {
     if (this.registerForm.valid) {
-      console.log("Register");
-      this.userService.register(this.registerForm.value);
+      this.userService.register(this.registerForm.value).subscribe({
+        next: (user: User) => {
+          console.log('User registered successfully:', user);
+        },
+        error: (error: any) => {
+          console.error('Error registering user:', error);
+        },
+      });;
     }
     else{
       console.log("NotValid");
     }
-    this.getUsers();
   }
 
-  // // Вход пользователя
-  // loginUser(): void {
-  //   if (this.loginForm.valid) {
-  //     this.http.post('/api/users/login', this.loginForm.value).subscribe(
-  //       (response: any) => {
-  //         this.message = `Добро пожаловать, ${response.user.name}`;
-  //       },
-  //       (error) => {
-  //         console.error('Ошибка входа:', error);
-  //         this.message = 'Ошибка входа: неверный логин или пароль';
-  //       }
-  //     );
-  //   }
-  // }
+  loginUser(): void{
+    if (this.loginForm.valid) {
+      this.userService.login(this.loginForm.value.login, this.loginForm.value.password).subscribe({
+        next: (response: any) => {
+          if (response.status_code == 200){
+            this.user_login= response.user.login;
+            console.log(this.user_login);
+            this.router.navigate([`messager/${this.loginForm.value.login}`]);
+          }
+        },
+        error: (error: any) => {
+          console.error('Error registering user:', error);
+        },
+      });;
+    }
+    else{
+      console.log("NotValid");
+    }
+  }
+
+  showRegistr(): void{
+    if (!this.registerDisplay){
+      this.loginDisplay = false;
+      this.registerDisplay = true;
+    }     
+  }
+  showLogin(): void{
+    if (!this.loginDisplay){
+      this.loginDisplay = true;
+      this.registerDisplay = false;
+    }     
+  }
 }
